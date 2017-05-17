fb789c7c0e4f3c942575a09315aef3421952fb6b
CherylYueWang <yue9@ualberta.ca>
2017-05-17 19:12:14 -0400

    update license

--- t1.py
+++ t1.py
@@ -1,3 +1,20 @@
+'''
+Copyright 2017 Yue Wang
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+'''
+
+
 import numpy as np
 import tensorflow as tf
 import random
@@ -71,9 +88,10 @@ def readData(filename, filetype):
 	for i in range(0, len(data)):
 		row = data[i]
 		if 'NA' in row:
-			print('hit')
+			#print('hit')
 			#data.remove(row)
 			#trainLabel.remove(trainLabel[i])
+			pass
 		else:
 			outputData.append([float(i) for i in row])
 			outputLabel.append(trainLabel[i])
@@ -94,7 +112,7 @@ def main():
 	print('total # of labels:',numLabels)
 
 	# casting
-	trainData = tf.to_float(outputData)
+	trainData = tf.to_float(outputData)#list to tensor
 	trainLabel = tf.to_float(trainLabel)
 
 	# Reference to tensorflow doc&tutorial
